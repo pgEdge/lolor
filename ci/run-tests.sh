77b8d5f1c84233c670a04389bcafc10fe485180f
Tej Kashi <pct960@gmail.com>
2024-06-20 13:19:13 -0400

    Adding circle-ci for lolor tests

---
+++ ci/run-tests.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# Golang tests
+
+#cd /home/pgedge/lolor/tests/go
+#go get github.com/jackc/pgx/v5
+#go get github.com/magiconair/properties
+#cp ../../ci/test-configs/test.properties.go test.properties
+#
+#go test -v
+#
+#
+IFS=',' read -r -a peer_names <<< "$PEER_NAMES"
+
+for PEER_HOSTNAME in "${peer_names[@]}";
+do
+  while ! pg_isready -h $PEER_HOSTNAME; do
+    echo "Waiting for $PEER_HOSTNAME..."
+    sleep 1
+  done
+done
+
+
+# Javascript tests
+cd /home/pgedge/lolor/tests/javascript
+cp ../../ci/test-configs/config.json config.json
+npm test
+
+# Junit tests
+#cd /home/pgedge/lolor/tests/junit/singlenode
+#cp ../../../ci/test-configs/test.properties.java-single test.properties
+#mvn test
+#
+#cd /home/pgedge/lolor/tests/junit/multinode
+#cp ../../../ci/test-configs/test.properties.java test.properties
+#mvn test
+
+# Python tests
+cd /home/pgedge/lolor/tests/python
+cp ../../ci/test-configs/test.properties.py test.properties
+python3 lolor_tests.py -v
---
+++ ci/test-configs/test.properties.go
@@ -0,0 +1,6 @@
+n1.url=postgres://admin:password@n1:5432/demo
+n2.url=postgres://admin:password@n2:5432/demo
+n3.url=postgres://admin:password@n3:5432/demo
+
+# sync delay (seconds)
+sync_delay=5
